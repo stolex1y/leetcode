09fa61e698a4c0648e6e15560cfca6bd66fc566e
Alexey Filimonov <stolexiy@mail.ru>
2022-07-22 13:12:19 +0300

    Refactoring with common parts

--- src/main/kotlin/ru/stolexiy/Common.kt
+++ src/main/kotlin/ru/stolexiy/Common.kt
@@ -1,5 +1,33 @@
 package ru.stolexiy
 
 class ListNode(var `val`: Int) {
+
+    companion object {
+        fun generateLinkedList(items: Iterable<Int>): ListNode? {
+            var head: ListNode? = null
+            var curr: ListNode? = null
+            items.forEachIndexed { index, value ->
+                if (index == 0) {
+                    head = ListNode(value)
+                    curr = head
+                } else {
+                    curr?.next = ListNode(value)
+                    curr = curr?.next
+                }
+            }
+            return head
+        }
+    }
+
     var next: ListNode? = null
+
+    fun toList(): List<Int> {
+        val list = mutableListOf<Int>()
+        var currNode: ListNode? = this
+        while (currNode != null) {
+            list += currNode.`val`
+            currNode = currNode.next
+        }
+        return list
+    }
 }
--- src/test/kotlin/ru/stolexiy/876) Middle of the Linked List.kt
+++ src/test/kotlin/ru/stolexiy/876) Middle of the Linked List.kt
@@ -3,26 +3,15 @@ package ru.stolexiy
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
+import ru.stolexiy.ListNode.Companion.generateLinkedList
 import kotlin.test.assertEquals
 
 class MiddleNodeTest {
 
-    private fun generateLinkedList(listSize: Int): ListNode? {
-        if (listSize == 0)
-            return null
-        val head = ListNode(1)
-        var curr = head
-        (2..listSize).forEach { value ->
-            curr.next = ListNode(value)
-            curr = curr.next!!
-        }
-        return head
-    }
-
     @ParameterizedTest
     @ValueSource(ints = [0, 1, 2, 5, 6, 10, 25, 100])
     internal fun test(listSize: Int) {
-        val list = generateLinkedList(listSize)
+        val list = generateLinkedList(1..listSize)
         val expectedVal = when (listSize) {
             0 -> null
             1 -> 1
