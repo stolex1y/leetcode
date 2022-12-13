6909e7ae92d8af3f673b275078fb2b7aab5175c1
Alexey Filimonov <stolexiy@mail.ru>
2022-12-13 23:47:46 +0300

    DS (String, Hash Table): 242, 383, 387

---
+++ src/main/kotlin/ru/stolexiy/387) First Unique Character in a String.kt
@@ -0,0 +1,13 @@
+package ru.stolexiy
+
+fun firstUniqChar(s: String): Int {
+    val charFreq = mutableMapOf<Char, Int>()
+    s.forEach {
+        charFreq[it] = charFreq.getOrDefault(it, 0) + 1
+    }
+    s.forEachIndexed { idx, ch ->
+        if (charFreq[ch] == 1)
+            return idx
+    }
+    return -1
+}
